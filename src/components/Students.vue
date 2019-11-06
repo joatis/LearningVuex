24bde5af60767a440155e6b3a568976723d886f0
John Oatis <joatis@joatis3.com>
2019-11-05 22:17:43 -0500

    Installed Vuex and modified app to use it.

--- src/components/Students.vue
+++ src/components/Students.vue
@@ -1,12 +1,11 @@
 <template>
  <v-flex sm8 offset-sm2>
-   {{ students }}
     <v-card>
       <v-toolbar dark>
         <v-toolbar-title>Students</v-toolbar-title>
       </v-toolbar>
       <v-list>
-        <v-list-tile v-for="(student, index) in students" :to="'editStudent/' + index">
+        <v-list-tile v-for="(student, index) in $store.state.students" :to="'editStudent/' + index">
           <v-list-tile-content>
             <v-list-tile-title v-text="student.name"></v-list-tile-title>
           </v-list-tile-content>
@@ -18,12 +17,10 @@
 
 <script>
 import axios from "axios";
-import store from "../store.js";
 
 export default {
     data() {
         return {
-            students: store.state.students,
         }
     },
   async created() {
--- src/store.js
+++ src/store.js
@@ -1,5 +1,10 @@
-export default {
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
     state: {
         students: []
     }
-}
+});
