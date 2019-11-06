98097540bcbc6a7b2080cbdf2975834562700ff0
John Oatis <joatis@joatis3.com>
2019-11-05 22:02:36 -0500

    Created our own store.
    For some reason I'm getting an error that is not in the
    video saying that store can't be referenced in Students

---
+++ src/components/Students.vue
@@ -0,0 +1,30 @@
+<template>
+ <v-flex sm8 offset-sm2>
+   {{ students }}
+    <v-card>
+      <v-toolbar dark>
+        <v-toolbar-title>Students</v-toolbar-title>
+      </v-toolbar>
+      <v-list>
+        <v-list-tile v-for="(student, index) in students" :to="'editStudent/' + index">
+          <v-list-tile-content>
+            <v-list-tile-title v-text="student.name"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            students: store.state.students,
+        }
+    },
+  async created() {
+  }
+};
+</script>
