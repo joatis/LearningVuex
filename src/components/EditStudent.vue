ec2125dcda6b6109aa8b4c4939928001b3e2506f
John Oatis <joatis@joatis3.com>
2019-11-08 22:49:46 -0500

    Using Getters

--- src/components/EditStudent.vue
+++ src/components/EditStudent.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    <v-flex sm8 offset-sm2>
+    <v-flex sm8 offset-sm2 >
       <v-card>
         <v-toolbar dark>
           <v-toolbar-title>Edit Student</v-toolbar-title>
         </v-toolbar>
-        <v-form>
+          <v-container class="text-xs-center">
+          <v-progress-circular v-if="!isLoaded" :size="70" :width="7" color="purple" indeterminate></v-progress-circular>
+          </v-container>
+        <v-form v-if="isLoaded">
           <v-container>
             <v-layout>
               <v-flex xs12 md4>
-                <v-text-field v-model="name" label="Name" required></v-text-field>
+                <v-text-field v-model="findStudent($route.params.id).firstName" label="First Name" required></v-text-field>
+                <v-text-field v-model="findStudent($route.params.id).lastName" label="Last Name" required></v-text-field>
               </v-flex>
             </v-layout>
           </v-container>
@@ -20,24 +24,33 @@
     <br>
     <Students/>
   </div>
+  
 </template>
 
 <script>
 import axios from "axios";
 import Students from "./Students";
+import { mapGetters } from 'vuex';
 
 export default {
   data() {
     return {
-      name: ""
     };
   },
-  async created() {
-      this.name = (await axios.get(`http://localhost:3000/students?id=${this.$route.params.id}`)).data[0].name;
+  created() {
+  },
+  computed: {
+    ...mapGetters([
+      'isLoaded',
+      'findStudent'
+    ])
   },
   methods: {
     async submit() {
-      axios.put(`http://localhost:3000/students/${this.$route.params.id}`, { name: this.name });
+      axios.put(`http://localhost:3000/students/${this.$route.params.id}`, {
+         firstName: this.student.firstName,
+         lastName: this.student.lastName 
+        });
     }
   },
   components: {
